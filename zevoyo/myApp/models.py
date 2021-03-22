78f726f2430118f53cc7f51b70d0e18c7ea5b611
Vibhushit07 <vibhushitsinghal80@gmail.com>
2021-03-22 19:38:50 +0530

    employee table created

--- zevoyo/myApp/models.py
+++ zevoyo/myApp/models.py
@@ -7,3 +7,9 @@ class Customer(models.Model):
     userName = models.CharField(max_length = 10)
     password = models.CharField(max_length = 20)
     phoneNumber = models.IntegerField()
+
+class Employee(models.Model):
+    custName = models.CharField(max_length = 20)
+    userName = models.CharField(max_length = 10)
+    password = models.CharField(max_length = 20)
+    phoneNumber = models.IntegerField()
