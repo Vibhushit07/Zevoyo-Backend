a2d913f67daae1b9b59dd17cd6d1c47b7a3c1a08
Vibhushit07 <vibhushitsinghal80@gmail.com>
2021-03-22 19:33:13 +0530

    customer table created

---
+++ zevoyo/myApp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Customer(models.Model):
+    custName = models.CharField(max_length = 20)
+    userName = models.CharField(max_length = 10)
+    password = models.CharField(max_length = 20)
+    phoneNumber = models.IntegerField()
