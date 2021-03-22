6957c26c9c1d9cf4cbda65af330728b46b7b9fd1
Vibhushit07 <vibhushitsinghal80@gmail.com>
2021-03-22 19:49:26 +0530

    hotel name, hotel employee and hotel rooms table created

--- zevoyo/myApp/models.py
+++ zevoyo/myApp/models.py
@@ -15,3 +15,19 @@ class Employee(models.Model):
     phoneNumber = models.IntegerField()
     isAdmin = models.CharField(max_length = 1)
 
+class HotelName(models.Model):
+    name = models.CharField(max_length = 20)
+    address = models.CharField(max_length = 50)
+    email = models.EmailField()
+
+class HotelEmployee(models.Model):
+    name = models.CharField(max_length = 20)
+    userName = models.CharField(max_length = 10)
+    password = models.CharField(max_length = 20)
+    phoneNumber = models.IntegerField()
+    hotelId = HotelName.pk
+
+class HotelRoom(models.Model):
+    hotelId = HotelName.pk
+    category = models.CharField(max_length = 10)
+    count = models.IntegerField()
