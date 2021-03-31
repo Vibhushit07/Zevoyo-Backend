03de82e21dff9e28029a550556b93fc3b48b0454
Vibhushit07 <vibhushitsinghal80@gmail.com>
2021-03-31 23:10:23 +0530

    home page

--- zevoyo/myApp/urls.py
+++ zevoyo/myApp/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.homePage),
+    path('getID/<int:id>',views.get_id),
+    path('getName/<str:empName>',views.get_name),
     path('register/',views.register_request,name="register"),
     path('login/',views.login_request,name="login"),
     path('getID/<int:id>',views.get_id),
