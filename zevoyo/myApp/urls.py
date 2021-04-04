100a7d4b408cf842b8d473a34ab0301c6ca258f8
Vibhushit07 <vibhushitsinghal80@gmail.com>
2021-04-04 22:33:55 +0530

    temp

--- zevoyo/myApp/urls.py
+++ zevoyo/myApp/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('getName/<str:empName>',views.get_name),
     path('register/',views.register_request,name="register"),
     path('login/',views.login_request,name="login"),
-    path('getID/<int:id>',views.get_id),
+    # path('getID/<int:id>',views.get_id),
     path('getName/<str:empName>',views.get_name),
     path('logout/', views.logoutUser, name="logout"),
     path('hotelDescription/', views.hotelDescription),
