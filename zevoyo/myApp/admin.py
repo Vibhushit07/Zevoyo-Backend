ae650fdbe6b68ed6bf3f3480c8908dfed68d4d0f
Jasmine-21 <jasmine171353.cse@chitkara.com>
2021-04-07 16:39:11 +0530

    created models of room booking

--- zevoyo/myApp/admin.py
+++ zevoyo/myApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Customer, Employee, HotelName, HotelRoom, HotelEmployee, Availability
+from .models import Customer,Booking, Employee, HotelName, HotelRoom, HotelEmployee, Availability
 
 # Register your models here.
 
@@ -8,4 +8,5 @@ admin.site.register(Employee)
 admin.site.register(HotelEmployee)
 admin.site.register(HotelName)
 admin.site.register(HotelRoom)
+admin.site.register(Booking)
 admin.site.register(Availability)
