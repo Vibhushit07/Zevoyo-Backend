794ef0715e5200275b5d1ea34349bb894a8716dd
Vibhushit07 <vibhushitsinghal80@gmail.com>
2021-04-11 01:49:14 +0530

    updated data

--- zevoyo/zevoyo/settings.py
+++ zevoyo/zevoyo/settings.py
@@ -39,7 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myApp.apps.MyappConfig',
+    
+    # third party
     'crispy_forms',
+    'phonenumber_field',
+
+    #own
+    'accounts',
+
 ]
 
 MIDDLEWARE = [
