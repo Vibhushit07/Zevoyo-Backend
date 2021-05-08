2990e32ec465a6e2eef9f02d592fcf1b8e09bcc0
Vibhushit07 <vibhushitsinghal80@gmail.com>
2021-05-08 14:21:20 +0530

    location app created

--- zevoyo/zevoyo/settings.py
+++ zevoyo/zevoyo/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myApp.apps.MyappConfig',
+    'location.apps.LocationConfig',
     
     # third party
     'crispy_forms',
