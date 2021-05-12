ac7b54e5490fedd4f974fe1390742b813e8e59f0
Vibhushit07 <vibhushitsinghal80@gmail.com>
2021-05-12 17:33:23 +0530

    filter booking initialized

---
+++ zevoyo/myApp/static/myApp/script/filterBookings.js
@@ -0,0 +1,25 @@
+function getDetails(c) {
+
+    var filter = document.getElementById('filter').value;
+
+    // $.ajax({
+    //     url: '/myApp/staff/searchDashboard/',
+    //     data: {
+    //         'filter': filter
+    //     },
+    //     dataType: 'json',
+    //     success: function(res) {
+
+    //         let parent = document.getElementById("data");
+    //         parent.innerHTML = "";
+
+    //         for (let i = 0; i < cities.length; i++) {
+
+    //             let child = document.createElement("option");
+    //             child.innerHTML = res[i].name;
+    //             parent.appendChild(child)
+    //         }
+
+    //     }
+    // });
+}
