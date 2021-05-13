2e245d8afbfaa5c20e18641879c9468388241c64
Vibhushit07 <vibhushitsinghal80@gmail.com>
2021-05-13 15:28:17 +0530

    fixed chat issue

---
+++ zevoyo/myApp/static/myApp/script/filter.js
@@ -0,0 +1,29 @@
+function getDetails(c) {
+
+    console.log(document.getElementById('filter').value)
+
+    var filter = document.getElementById('filter').value;
+
+    console.log(filter)
+
+    $.ajax({
+        url: '/myApp/staff/allbookings/filter/',
+        data: {
+            'filter': filter
+        },
+        dataType: 'json',
+        success: function(res) {
+
+            let parent = document.getElementById("data");
+            parent.innerHTML = "";
+
+            for (let i = 0; i < res.length; i++) {
+
+                let child = document.createElement("option");
+                child.innerHTML = res[i];
+                parent.appendChild(child)
+            }
+
+        }
+    });
+}
