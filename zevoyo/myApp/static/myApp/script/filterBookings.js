2533b7f5c3140a76302f71a4347478c28ecef4db
Vibhushit07 <vibhushitsinghal80@gmail.com>
2021-05-21 19:14:45 +0530

    fixed bug in header

---
+++ zevoyo/myApp/static/myApp/script/filterBookings.js
@@ -0,0 +1,43 @@
+function getDetails(c) {
+
+    var filter = document.getElementById('filter').value;
+
+    let parent = document.getElementById("div-data");
+    parent.innerHTML = "";
+
+    if (filter === 'checkIn' || filter === 'checkOut') {
+
+        var input = document.createElement("input");
+        input.setAttribute("id", "data");
+        input.setAttribute("type", "date");
+        input.setAttribute("name", "data");
+        parent.appendChild(input);
+
+    } else {
+
+        $.ajax({
+            url: '/myApp/staff/allbookings/filter/',
+            data: {
+                'filter': filter
+            },
+            dataType: 'json',
+            success: function(res) {
+
+                let select = document.createElement("select");
+                select.setAttribute("class", "form-control");
+                select.setAttribute("id", "data");
+                select.setAttribute("name", "data");
+
+                for (let i = 0; i < res.length; i++) {
+
+                    let child = document.createElement("option");
+                    child.innerHTML = res[i];
+                    child.setAttribute("value", res[i])
+                    select.appendChild(child)
+                }
+
+                parent.appendChild(select);
+            }
+        });
+    }
+}
