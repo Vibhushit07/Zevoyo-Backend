385d38daded166cfeaa214ac868c7e7f886452b2
Vibhushit07 <vibhushitsinghal80@gmail.com>
2021-06-01 14:24:32 +0530

    fixed unwanted chat in admin issue

--- zevoyo/chat/views.py
+++ zevoyo/chat/views.py
@@ -1,3 +1,4 @@
+from django.db.models.fields import AutoField
 from django.shortcuts import render, redirect
 from django.views.generic import CreateView, ListView
 from django.http import HttpResponse
@@ -44,13 +45,18 @@ def newChat(request):
 @login_required(login_url = "/staff")
 def chatList(request):
 
-    user = ""
+    user = User.objects.all().get(id = request.user.id)
     chat = []
+
+    admin = 0
 
     if request.method == "POST" and request.user.is_staff:
         user = User.objects.all().get(id = request.GET['userid'].split('/')[0])   
-    else:
-        user = User.objects.all().get(id = request.user.id)
+    elif request.user.is_staff:
+        admin = 1
+        # user = User.objects.all().get(id = request.user.id)
+    # else:
+        # user = User.objects.all().get(id = request.user.id)
 
     for c in Chat.objects.filter(user = user):
         chat.append(c)
@@ -62,7 +68,7 @@ def chatList(request):
 
     userList = User.objects.all().filter(is_staff = False)
 
-    return HttpResponse(render(request, 'chatAll.html', { 'chatAll': chat, 'users': userList }))
+    return HttpResponse(render(request, 'chatAll.html', { 'chatAll': chat, 'users': userList, 'admin': admin }))
 
 def posted_at(e):
   return e.posted_at
