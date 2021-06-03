a3eb621a4c65f1b1b07b21a5fa853297af0a3172
Vibhushit07 <vibhushitsinghal80@gmail.com>
2021-06-03 15:30:12 +0530

    updated status value in dashboard bookings

--- zevoyo/myApp/static/myApp/script/filterBookings.js
+++ zevoyo/myApp/static/myApp/script/filterBookings.js
@@ -1,3 +1,85 @@
+$("#filter").click(function() {
+
+    var filter = document.getElementById('filter').value;
+
+    let parent = document.getElementById("div-data");
+    parent.innerHTML = "";
+
+    if (filter === 'checkIn' || filter === 'checkOut') {
+
+        var input = document.createElement("input");
+        input.setAttribute("id", "data");
+        input.setAttribute("type", "date");
+        input.setAttribute("name", "data");
+        parent.append(input);
+
+    } else if (filter === 'status') {
+
+        let select = document.createElement("select");
+        select.setAttribute("class", "form-control");
+        select.setAttribute("id", "data");
+        select.setAttribute("name", "data");
+
+        let res = ['Available', 'Not Available']
+
+        for (let i = 0; i < res.length; i++) {
+
+            let child = document.createElement("option");
+            child.innerHTML = res[i];
+            child.setAttribute("value", i + 1)
+            select.append(child)
+        }
+
+        parent.append(select);
+
+    } else if (filter === 'roomType') {
+
+        let select = document.createElement("select");
+        select.setAttribute("class", "form-control");
+        select.setAttribute("id", "data");
+        select.setAttribute("name", "data");
+
+        let res = ['Premium', 'Deluxe', 'Basic']
+
+        for (let i = 0; i < res.length; i++) {
+
+            let child = document.createElement("option");
+            child.innerHTML = res[i];
+            child.setAttribute("value", i + 1)
+            select.append(child)
+        }
+
+        parent.append(select);
+
+    } else {
+
+        $.ajax({
+            url: '/myApp/staff/filter/',
+            data: {
+                'filter': filter
+            },
+            dataType: 'json',
+            success: function(res) {
+
+                let select = document.createElement("select");
+                select.setAttribute("class", "form-control");
+                select.setAttribute("id", "data");
+                select.setAttribute("name", "data");
+
+                for (let i = 0; i < res.length; i++) {
+
+                    let child = document.createElement("option");
+                    child.innerHTML = res[i];
+                    child.setAttribute("value", res[i])
+                    select.append(child)
+                }
+
+                parent.append(select);
+            }
+        });
+    }
+})
+
 function getDetails(c) {
 
     var filter = document.getElementById('filter').value;
