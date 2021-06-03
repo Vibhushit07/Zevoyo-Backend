b4dd8d5cbb6549a64e2da031f3134b559af4d187
Vibhushit07 <vibhushitsinghal80@gmail.com>
2021-06-03 09:52:02 +0530

    updated url

--- zevoyo/myApp/static/myApp/script/dashboard.js
+++ zevoyo/myApp/static/myApp/script/dashboard.js
@@ -19,4 +19,68 @@ $(function() {
         return percentage / 100 * 360
 
     }
+
+})
+
+$("#filter").click(function() {
+
+    var filter = document.getElementById('filter').value;
+
+    let parent = document.getElementById("div-data");
+    parent.innerHTML = "";
+
+    if (filter === 'checkIn' || filter === 'checkOut') {
+
+        var input = document.createElement("input");
+        input.setAttribute("id", "data");
+        input.setAttribute("type", "date");
+        input.setAttribute("name", "data");
+        parent.append(input);
+
+    } else if (filter === 'status') {
+
+        let select = document.createElement("select");
+        select.setAttribute("class", "form-control");
+        select.setAttribute("id", "data");
+        select.setAttribute("name", "data");
+
+        let res = ['Available', 'Not Available']
+
+        for (let i = 0; i < res.length; i++) {
+
+            let child = document.createElement("option");
+            child.innerHTML = res[i];
+            child.setAttribute("value", i + 1)
+            select.append(child)
+        }
+
+        parent.append(select);
+
+    } else {
+
+        $.ajax({
+            url: '/myApp/staff/allbookings/filter/',
+            data: {
+                'filter': filter
+            },
+            dataType: 'json',
+            success: function(res) {
+
+                let select = document.createElement("select");
+                select.setAttribute("class", "form-control");
+                select.setAttribute("id", "data");
+                select.setAttribute("name", "data");
+
+                for (let i = 0; i < res.length; i++) {
+
+                    let child = document.createElement("option");
+                    child.innerHTML = res[i];
+                    child.setAttribute("value", res[i])
+                    select.append(child)
+                }
+
+                parent.append(select);
+            }
+        });
+    }
 })
