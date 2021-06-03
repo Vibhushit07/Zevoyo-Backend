0a581665ceef57777b10f3187558e1dd9b4d0e91
Vibhushit07 <vibhushitsinghal80@gmail.com>
2021-06-03 15:30:50 +0530

    updated status value in dashboard and bookings

--- zevoyo/myApp/static/myApp/script/dashboard.js
+++ zevoyo/myApp/static/myApp/script/dashboard.js
@@ -22,84 +22,84 @@ $(function() {
 
 })
 
-$("#filter").click(function() {
+// $("#filter").click(function() {
 
-    var filter = document.getElementById('filter').value;
+//     var filter = document.getElementById('filter').value;
 
-    let parent = document.getElementById("div-data");
-    parent.innerHTML = "";
+//     let parent = document.getElementById("div-data");
+//     parent.innerHTML = "";
 
-    if (filter === 'checkIn' || filter === 'checkOut') {
+//     if (filter === 'checkIn' || filter === 'checkOut') {
 
-        var input = document.createElement("input");
-        input.setAttribute("id", "data");
-        input.setAttribute("type", "date");
-        input.setAttribute("name", "data");
-        parent.append(input);
+//         var input = document.createElement("input");
+//         input.setAttribute("id", "data");
+//         input.setAttribute("type", "date");
+//         input.setAttribute("name", "data");
+//         parent.append(input);
 
-    } else if (filter === 'status') {
+//     } else if (filter === 'status') {
 
-        let select = document.createElement("select");
-        select.setAttribute("class", "form-control");
-        select.setAttribute("id", "data");
-        select.setAttribute("name", "data");
+//         let select = document.createElement("select");
+//         select.setAttribute("class", "form-control");
+//         select.setAttribute("id", "data");
+//         select.setAttribute("name", "data");
 
-        let res = ['Available', 'Not Available']
+//         let res = ['Available', 'Not Available']
 
-        for (let i = 0; i < res.length; i++) {
+//         for (let i = 0; i < res.length; i++) {
 
-            let child = document.createElement("option");
-            child.innerHTML = res[i];
-            child.setAttribute("value", i + 1)
-            select.append(child)
-        }
+//             let child = document.createElement("option");
+//             child.innerHTML = res[i];
+//             child.setAttribute("value", i + 1)
+//             select.append(child)
+//         }
 
-        parent.append(select);
+//         parent.append(select);
 
-    } else if (filter === 'roomType') {
+//     } else if (filter === 'roomType') {
 
-        let select = document.createElement("select");
-        select.setAttribute("class", "form-control");
-        select.setAttribute("id", "data");
-        select.setAttribute("name", "data");
+//         let select = document.createElement("select");
+//         select.setAttribute("class", "form-control");
+//         select.setAttribute("id", "data");
+//         select.setAttribute("name", "data");
 
-        let res = ['Premium', 'Deluxe', 'Basic']
+//         let res = ['Premium', 'Deluxe', 'Basic']
 
-        for (let i = 0; i < res.length; i++) {
+//         for (let i = 0; i < res.length; i++) {
 
-            let child = document.createElement("option");
-            child.innerHTML = res[i];
-            child.setAttribute("value", i + 1)
-            select.append(child)
-        }
+//             let child = document.createElement("option");
+//             child.innerHTML = res[i];
+//             child.setAttribute("value", i + 1)
+//             select.append(child)
+//         }
 
-        parent.append(select);
+//         parent.append(select);
 
-    } else {
+//     } else {
 
-        $.ajax({
-            url: '/myApp/staff/filter/',
-            data: {
-                'filter': filter
-            },
-            dataType: 'json',
-            success: function(res) {
+//         $.ajax({
+//             url: '/myApp/staff/filter/',
+//             data: {
+//                 'filter': filter
+//             },
+//             dataType: 'json',
+//             success: function(res) {
 
-                let select = document.createElement("select");
-                select.setAttribute("class", "form-control");
-                select.setAttribute("id", "data");
-                select.setAttribute("name", "data");
+//                 let select = document.createElement("select");
+//                 select.setAttribute("class", "form-control");
+//                 select.setAttribute("id", "data");
+//                 select.setAttribute("name", "data");
 
-                for (let i = 0; i < res.length; i++) {
+//                 for (let i = 0; i < res.length; i++) {
 
-                    let child = document.createElement("option");
-                    child.innerHTML = res[i];
-                    child.setAttribute("value", res[i])
-                    select.append(child)
-                }
+//                     let child = document.createElement("option");
+//                     child.innerHTML = res[i];
+//                     child.setAttribute("value", res[i])
+//                     select.append(child)
+//                 }
 
-                parent.append(select);
-            }
-        });
-    }
-})
+//                 parent.append(select);
+//             }
+//         });
+//     }
+// })
