6fed0dbbf140398017af52ba1d102f1cab6b68d2
Vibhushit07 <vibhushitsinghal80@gmail.com>
2021-06-05 16:00:02 +0530

    rebasing

--- zevoyo/myApp/static/myApp/script/filter.js
+++ zevoyo/myApp/static/myApp/script/filter.js
@@ -23,6 +23,15 @@ $("#filter").click(function() {
         input.setAttribute("value", "allBookings");
         parent.append(input);
 
+    } else if (filter === 'allRooms') {
+
+        var input = document.createElement("input");
+        // input.setAttribute("id", "data");
+        input.setAttribute("type", "hidden");
+        input.setAttribute("name", "data");
+        input.setAttribute("value", "allRooms");
+        parent.append(input);
+
     } else {
 
         $.ajax({
