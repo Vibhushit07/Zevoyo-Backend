8806f14e625b92f9ffb27ac9d645a1a00ed464b2
Vibhushit07 <vibhushitsinghal80@gmail.com>
2021-06-05 16:03:53 +0530

    rebasing

--- zevoyo/myApp/views.py
+++ zevoyo/myApp/views.py
@@ -265,7 +265,7 @@ def dashboard(request):
             elif(filter == "roomType"):
                 rooms = Rooms.objects.filter(roomType = data).order_by("hotel__city", "hotel__name")
             
-            elif(filter == "status"):
+            elif(filter == "statusRoom"):
                 rooms = Rooms.objects.filter(status = data).order_by("hotel__city", "hotel__name")
       
     response = render(request, 'staff/dashboard.html', {'cities': cities, 'reserved': reserved, 'rooms': rooms, 'totalRooms': totalRooms, 'available': availableRooms, 'unavailable': unavailableRooms, 'bookings': bookings})
